fb69864db644e693157c2013cd314899c7aa4767
Philippe-Henri <paulphilippeh@gmail.com>
2026-02-03 12:03:23 +0100

    pour mes amours

--- src/classicfit/ClassicFit.java
+++ src/classicfit/ClassicFit.java
@@ -13,9 +13,7 @@ package classicfit;
  */
 public class ClassicFit {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
